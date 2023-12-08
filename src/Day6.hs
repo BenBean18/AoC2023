9f03a2052ff85471219d6c719d6a28c74af77509
BenBean18 <beakerthespeaker@gmail.com>
2023-12-08 18:28:46 -0500

    Day 6 done
    part 2 i initially did with manual input manipulation
    but regex is in the comments and i'll do it later

---
+++ src/Day6.hs
@@ -0,0 +1,56 @@
+module Day6 where
+
+import Utilities
+import Data.List.Split
+import Data.List
+import Data.Text (pack, unpack, replace, isInfixOf)
+import Text.Regex.Base
+import Text.Regex.PCRE
+import Data.Array ((!))
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import Debug.Trace
+import Criterion.Main
+import System.Environment
+import Data.Maybe
+
+-- it's accelerating for n milliseconds but not moving, then just moving forward at a specific velocity
+-- so we just calculate final velocity
+
+-- https://www.desmos.com/calculator/lvvmk043dk
+
+data Race = Race { time :: Int, distance :: Int } deriving (Ord, Show, Eq)
+
+parseInts :: String -> [Int]
+parseInts str = map (\s -> read s :: Int) (tail $ wordsBy (== ' ') str)
+
+intsToRace :: (Int, Int) -> Race
+intsToRace (time, distance) = Race { time = time, distance = distance }
+
+parseRaces :: [String] -> [Race]
+parseRaces [times, distances] = zipWith (curry intsToRace) (parseInts times) (parseInts distances)
+
+minTime :: Race -> Int
+minTime Race { time = tInt, distance = dInt } =
+    let t = fromIntegral tInt :: Double
+        d = fromIntegral dInt :: Double in floor ((-t + sqrt ((t ** 2) - (4 * d))) / (-2))
+
+maxTime :: Race -> Int
+maxTime Race { time = tInt, distance = dInt } =
+    let t = fromIntegral tInt :: Double
+        d = fromIntegral dInt :: Double in floor ((-t - sqrt ((t ** 2) - (4 * d))) / (-2))
+
+numberOfWays :: Race -> Int
+numberOfWays race = maxTime race - minTime race
+
+part1' lines = 
+    let result = product $ map numberOfWays (parseRaces lines) in print result
+
+part1 = do
+    lines <- getLines "day6/input.txt"
+    text <- getText "day6/input.txt"
+    part1' lines
+
+-- Part 2 is part 1 but you do s/(\d) + (\d)/$1$2
+
+part2 = print "Hi"
--- src/Main.hs
+++ src/Main.hs
@@ -6,6 +6,7 @@ import qualified Day2
 import qualified Day3
 import qualified Day4
 import qualified Day5
+import qualified Day6
 
 main :: IO ()
 main = do
@@ -19,6 +20,7 @@ main = do
                     else if day == 3 then Day3.part1
                     else if day == 4 then Day4.part1
                     else if day == 5 then Day5.part1
+                    else if day == 6 then Day6.part1
                     else putStrLn "I haven't solved that yet (or it doesn't exist)"
                 else if part == "2" then
                     if day == 1 then Day1.part2
@@ -26,6 +28,7 @@ main = do
                     else if day == 3 then Day3.part2
                     else if day == 4 then Day4.part2
                     else if day == 5 then Day5.part2
+                    else if day == 6 then Day6.part2
                     else putStrLn "I haven't solved that yet (or it doesn't exist)"
                 else if part == "b" then
                     if day == 1 then Day1.benchmark
