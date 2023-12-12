efa025d1696e1e2ca40f2cad9681b60d5b45da6b
BenBean18 <beakerthespeaker@gmail.com>
2023-12-11 22:18:38 -0500

    Day 5 Part 2 in progress

--- src/Day5_part2.hs
+++ src/Day5_part2.hs
@@ -22,22 +22,42 @@ data Range = Range Int Int deriving (Ord, Show, Eq) -- start, end INCLUSIVE ON B
 clamp :: (Ord a) => a -> a -> a -> a
 clamp mn mx = max mn . min mx
 
-correspondingRange :: Range -> RangeMap -> [RangeMap]
-correspondingRange (Range start end) (Range keyStart keyEnd, Range valStart valEnd) = 
-    let boundStart = clamp keyStart keyEnd start
-        boundEnd = clamp keyStart keyEnd end
-        newRange = (Range boundStart boundEnd, Range (valStart + boundStart - keyStart) (valStart + boundStart - keyStart + (boundEnd - boundStart))) in
-            (if keyStart < boundStart then [(Range keyStart boundStart, Range valStart (valStart + boundStart - keyStart))] else []) ++ [newRange] ++
-            (if keyEnd > boundEnd then [(Range boundEnd keyEnd, Range (valStart + (boundEnd - keyStart)) valEnd)] else [])
+rangeLength :: Range -> Int
+rangeLength (Range start end) = abs (end - start)
+
+rangeIntersection :: Range -> Range -> Range
+rangeIntersection (Range s1 e1) (Range s2 e2) =
+    if s2 > e1 || s1 > e2 then (Range 0 0) else Range (max s1 s2) (min e1 e2)
+
+isWithin :: Range -> Int -> Bool
+isWithin (Range start end) i = start <= i && i <= end
+
+-- correspondingRange :: Range -> (Range, Range) -> [(Range, Range)]
+-- correspondingRange (Range start end) (Range keyStart keyEnd, Range valStart valEnd) = 
+--     let boundStart = clamp keyStart keyEnd start
+--         boundEnd = clamp keyStart keyEnd end
+--         newRange = (Range boundStart boundEnd, Range (valStart + boundStart - keyStart) (valStart + boundStart - keyStart + (boundEnd - boundStart))) in
+--             (if keyStart < boundStart then [(Range keyStart boundStart, Range valStart (valStart + boundStart - keyStart))] else []) ++ [newRange] ++
+--             (if keyEnd > boundEnd then [(Range boundEnd keyEnd, Range (valStart + (boundEnd - keyStart)) valEnd)] else [])
+
+-- must be in bounds
+correspondingRange :: Range -> (Range, Range) -> Range
+correspondingRange (Range start end) (Range keyStart keyEnd, Range valStart valEnd) =
+    if (rangeLength (rangeIntersection (Range start end) (Range keyStart keyEnd))) == 0 then (Range 0 0) else
+        Range (valStart + (start - keyStart)) (valStart + (end - keyStart))
+
+applyMap :: Range -> RangeMap -> [Range]
+applyMap range rangeMap =
+    let keys = Map.keys rangeMap
+        vals = Map.elems rangeMap
+        intersectingRanges = map (rangeIntersection range) keys
+        mappedRanges = filter (\r -> rangeLength r /= 0) (concat (map (\inter -> map (correspondingRange inter) (zip keys vals)) (filter (\r -> rangeLength r /= 0) intersectingRanges))) in mappedRanges
+
+
 
 -- applyMap :: RangeMap -> RangeMap -> [RangeMap]
 -- applyMap existing new = 
 
-type RangeMap = (Range, Range) -- key, value
-
--- e.g. we have the range [1,2,3] -> [4,5,6], [4,5,6] -> [1,2,3]
--- and we get the input [3,4,5]
--- so we need to get the intersection of [3,4,5] with each of the keys
--- which should yield [3,6]
+type RangeMap = Map.Map Range Range
 
 part2' lines = print "Hi"
