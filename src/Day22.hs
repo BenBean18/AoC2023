7d0ea8bd385efea0a799ce7835dc4b7046016c48
BenBean18 <beakerthespeaker@gmail.com>
2023-12-22 16:46:27 +0000

    day 22 theoretically works (this is from my phone using iSH)

--- src/Day22.hs
+++ src/Day22.hs
@@ -39,12 +39,43 @@ parseBricks strs = map (parseBrick strs) [0..length strs-1]
 sortBricks :: [Brick] -> [Brick]
 sortBricks = sortBy (\brick1 brick2 -> compare (fst (z brick1)) (fst (z brick2)))
 
-spacesUnder :: Brick -> Grid -> Brick
-spacesUnder Brick { x = (x1,x2), y = (y1,y2), z = (z1,z2), i = index } = -- filter map's keys
+spacesUnder :: Brick -> Grid -> Grid
+spacesUnder Brick { x = (x1,x2), y = (y1,y2), z = (z1,z2), i = index } =
+    Map.filter (\(x,y,z) -> x1 <= x && x <= x2 && y1 <= y && y <= y2 && z1 <= z && z <= z2)
+
+-- Returns (z, [brick IDs])
+highestBricks :: Grid -> (Int, [Int])
+highestBricks grid =
+    let highestZ = maximum (1 : (map (\(x,y,z) -> z) (Map.keys grid)))
+        highestBrickIDs = Map.elems (Map.filter (\(x,y,z) -> z == highestZ) grid) in (highestZ, highestBrickIDs)
+
+insertBrick :: Grid -> Brick -> Int -> Grid
+insertBrick grid Brick { x = (x1,x2), y = (y1,y2), z = (z1,z2), i = index } insertionZ =
+    let allX = [x1..x2]
+        allY = [y1..y2]
+        allZ = [0+insertionZ..(z2-z1)+insertionZ]
+        allCoords = map (\x -> map (\y -> map (\z -> (x,y,z)) allZ) allY) allX -- yay 3D "for loop" in
+    foldl (\m coord -> Map.insert coord index m) grid allCoords
+
+-- returns (new grid, [supporting brick IDs])
+insertFallingBrick :: Grid -> Brick -> (Grid, [Int])
+insertFallingBrick grid brick =
+    let (highestZ, ids) = highestBricks (spacesUnder brick grid) in (insertBrick grid brick (highestZ + 1), ids)
+
+-- bricks MUST be sorted lowest to highest Z
+makeBricksFall :: [Brick] -> (Grid, Set.Set Int)
+makeBricksFall bricks = foldl (\(grid, supporting) brick -> (fst (insertFallingBrick grid brick), Set.union supporting (Set.fromList (snd (insertFallingBrick grid brick))))) (Map.empty, Set.empty) bricks
 
 part1' lines = 
     let bricks = parseBricks lines
-        sortedBricks = sortBricks bricks in print sortedBricks
+        sortedBricks = sortBricks bricks
+        (fallenState, supportingBricks) = makeBricksFall sortedBricks
+        safeToDisintegrate = length bricks - (Set.size supportingBricks) in do
+    print fallenState
+    print supportingBricks
+    putStrLn "-------------------"
+    print safeToDisintegrate
+
 
 part1 = do
     lines <- getLines "day22/input.txt"
