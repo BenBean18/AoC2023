acdbb5e4c238aaad7ce4c7ff2f0f8d3aa2cee23b
BenBean18 <beakerthespeaker@gmail.com>
2023-12-22 11:10:53 -0500

    day 22 backup

--- src/Day22.hs
+++ src/Day22.hs
@@ -18,9 +18,33 @@ import qualified Data.MultiSet as MultiSet
 import Data.List.Unique (allUnique)
 
 -- Part 1
+-- Strategy: sort bricks by their Z coordinate
+-- While there are bricks left, move the lowest Z bricks down as far as they can go (on top of the current highest Z block in the span of the brick)
+-- Mark each "claimed" coordinate with the index of the brick
 
+-- x,y,z all inclusive ranges
+data Brick = Brick { x :: (Int, Int), y :: (Int, Int), z :: (Int, Int), i :: Int } deriving (Eq, Show, Ord)
+type Grid = Map.Map (Int,Int,Int) Int -- :( the int is for which brick the spot is claimed by
 
-part1' lines = print "Hi"
+parseBrick :: [String] -> Int -> Brick
+parseBrick strs index = 
+    let s = strs !! index
+        [start,end] = splitOn "~" s
+        [x1,y1,z1] = map (\num -> read num :: Int) (splitOn "," start)
+        [x2,y2,z2] = map (\num -> read num :: Int) (splitOn "," end) in Brick { x = (x1,x2), y = (y1,y2), z = (z1,z2), i = index }
+
+parseBricks :: [String] -> [Brick]
+parseBricks strs = map (parseBrick strs) [0..length strs-1]
+
+sortBricks :: [Brick] -> [Brick]
+sortBricks = sortBy (\brick1 brick2 -> compare (fst (z brick1)) (fst (z brick2)))
+
+spacesUnder :: Brick -> Grid -> Brick
+spacesUnder Brick { x = (x1,x2), y = (y1,y2), z = (z1,z2), i = index } = -- filter map's keys
+
+part1' lines = 
+    let bricks = parseBricks lines
+        sortedBricks = sortBricks bricks in print sortedBricks
 
 part1 = do
     lines <- getLines "day22/input.txt"
